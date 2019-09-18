b0585410f0366f07a023b400a0aa655aa4903057
parvezsultana <55456754+parvezsultana@users.noreply.github.com>
2019-09-18 19:36:50 +0530

    Added task2

---
+++ task2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num;
+	cout<<"Enter number:";
+	cin>>num;
+	if(num%2==0)
+	{
+		cout<<num<<"is even number";
+	}
+	else
+	{
+		cout<<num<<"is odd number";
+	}
+}
