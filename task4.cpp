e67f7fc1bc8c4075e7b63ea386e8bd2386a27431
parvezsultana <55456754+parvezsultana@users.noreply.github.com>
2019-09-18 20:11:31 +0530

    Added task4

--- task4.cpp
+++ task4.cpp
@@ -1,25 +1,14 @@
 #include<iostream>
-
 using namespace std;
-
-main()
-
+int main()
 {
-
 int a[10],sum=0;
-
 cout<<"enter 10 elements";
-
-for(int i=0;i<=10;i++)
-
+for(int i=0;i<10;i++)
 {
-
-cin>>i;
-
-sum=sum+i;
-
+cin>>a[i];
+sum=sum+a[i];
 }
-
 cout<<"sum of elements is"<<sum;
-
+  return(0);
 }
